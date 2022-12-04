bd50aa6340f27c4f344f0f04531ad753b24a5f35
Franck Trouillez <francktrouillez99@gmail.com>
2022-12-04 02:23:01 +0100

    Refactor solution day 1

--- day1/Solution.rb
+++ day1/Solution.rb
@@ -3,21 +3,15 @@ require_relative "../helpers/Solution"
 module Day01
   class Solution < ::Solution
     def generate_output_1
-      compute.max
+      calories.max
     end
 
     def generate_output_2
-      compute.sort.reverse[...3].sum
+      calories.sort.reverse[...3].sum
     end
 
-    def compute
-      calories = [0]
-      input.each do |input_line|
-        next calories[-1] += input_line.to_i unless input_line.empty?
-
-        calories.append(0)
-      end
-      calories
+    def calories
+      input.reduce([0]) { |acc, i| i.empty? ? acc + [0] : (acc[-1] += i.to_i) && acc }
     end
   end
 end
