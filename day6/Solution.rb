1c7b7b0205937910b6178a638b3ace9e7af5a69a
Franck Trouillez <francktrouillez99@gmail.com>
2022-12-08 01:34:46 +0100

    Refactor find_start_of_packet to start_of_packet

--- day6/Solution.rb
+++ day6/Solution.rb
@@ -3,14 +3,14 @@ require_relative "../helpers/Solution"
 module Day06
   class Solution < ::Solution
     def generate_output_1
-      find_start_of_packet 4
+      start_of_packet 4
     end
 
     def generate_output_2
-      find_start_of_packet 14
+      start_of_packet 14
     end
 
-    def find_start_of_packet(size)
+    def start_of_packet(size)
       input.first.split("").each_cons(size).to_a.index { |e| e.uniq.length == size } + size
     end
   end
